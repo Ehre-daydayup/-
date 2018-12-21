922bd86e4e75fa94e60c1ef32472af1732f5d9dc
wzhouen <46052413+wzhouen@users.noreply.github.com>
2018-12-22 00:04:36 +0800

    Create ReadSelf.java

---
+++ ReadSelf.java
@@ -0,0 +1,88 @@
+package pkg;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ReadSelf extends JFrame implements ActionListener{
+	JPanel p1 = new JPanel();
+	JPanel p2 = new JPanel();
+	JPanel p3 = new JPanel();
+
+	JLabel head = new JLabel("个人信息修改");
+	JLabel l1 = new JLabel("管理员号：");
+	JLabel l2 = new JLabel("登录密码：");
+
+	JTextField t1 = new JTextField(10);
+	JTextField t2 = new JTextField(10);
+
+	JButton b1 = new JButton("确定");
+	JButton b2 = new JButton("取消");
+	
+	String s1,s2;
+	
+	Conn a=new Conn();
+
+	public ReadSelf() {
+		super("图书管理系统");
+		setLayout(new FlowLayout());
+
+		p2.setLayout(new GridLayout(2, 2));
+
+		p1.add(head);
+
+		p2.add(l1);
+		p2.add(t1);
+		p2.add(l2);
+		p2.add(t2);
+
+		p3.add(b1);
+		p3.add(b2);
+		
+		add(p1);
+		add(p2);
+		add(p3);
+
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+	}
+	
+	public void display() {
+		t1.setText(s1);
+		t2.setText(s2);
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		System.out.println(s1+s2);
+		
+	}
+	
+	public void setString(String s1,String s2) {
+		this.s1=s1;
+		this.s2=s2;
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(b1)) {
+			a.connDB();
+		    boolean b=false;
+			try {
+				b=a.stmt.execute("update  读者 set 读者编号=\'"+t1.getText().trim()+
+						"\',登录密码=\'"+ t2.getText().trim()+"\' where 读者编号=\'"+s1+"\'");
+				this.dispose();
+			}catch(Exception e1) {
+				e1.printStackTrace();
+			}
+			
+		}
+		if(e.getSource().equals(b2)) {
+			this.dispose();
+		}
+	}
+
+	public static void main(String args[]) {
+		new ReadSelf();
+	}
+}
